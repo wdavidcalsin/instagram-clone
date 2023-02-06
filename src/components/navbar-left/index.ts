7cbc1aa9a0bd21b7e86b9ad94399f427d3751017
WDavid <wdavidcalsin968@gmail.com>
2023-02-06 16:25:34 -0500

    Rename PascalCase file name to snake_case by Vercel file name principle

--- src/components/navbar-left/index.ts
+++ src/components/navbar-left/index.ts
@@ -1,4 +1,4 @@
-export { default as NavbarLeft } from './navbar-left';
+export { default as NavbarLeft } from './navbarLeft';
 export { default as NavbarLeftDesktop } from './navbar-left-desktop';
 export { default as NavbarLeftMobile } from './navbar-left-mobile';
 export * from './components';
